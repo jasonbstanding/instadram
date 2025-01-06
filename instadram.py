0bc83587657b4e5f33fd05377431af3b8a91ca56
Jason B. Standing <boondoggle@gimcrack.org>
2025-01-06 00:53:22 +0000

    Rework script to only download/process files once and cleanup afterwards

--- instadram.py
+++ instadram.py
@@ -1,17 +1,22 @@
-from dotenv import load_dotenv
-import os
-import instaloader
-import json
-import requests
-import lzma
 import base64
-import re
-import random
+from datetime import datetime
+from dotenv import load_dotenv
+import instaloader
+from instaloader import Profile
 import logging
+import lzma
+import os
+import random
+import re
+import requests
+import shutil
+import traceback
 
 # File to store the last downloaded post ID
-LAST_POST_FILE = "last_post_id.txt"
+LAST_POST_FILE = "last_post_date.txt"
 
+# How many posts to pull down each time - 0 for all
+POSTS_PER_INVOKE = 20
 
 # Load environment variables from the .env file
 load_dotenv(override=True)
@@ -32,11 +37,8 @@ wp_username = os.getenv("WP_USER")
 wp_application_password = os.getenv("WP_PASS")
 
 
-
 # Initialize variables
-new_posts_downloaded = 0
-last_fetched_post_id = None
-
+last_post_date = None
 
 def extractfiles(path):
     for file in os.listdir(path):
@@ -51,77 +53,40 @@ def extractfiles(path):
             logging.info(f"Decompressed {input_file}")
 
 
-def get_last_post_id():
-    """Read the last post ID from the file."""
+def get_last_post_date():
+    """Read the last post date from the file."""
     if os.path.exists(LAST_POST_FILE):
         with open(LAST_POST_FILE, "r") as file:
-            return file.read().strip()
+            return datetime.strptime(file.read().strip(), '%Y-%m-%d %H:%M:%S')
     return None
 
-def save_last_post_id(post_id):
-    """Save the last downloaded post ID to the file."""
+def save_last_post_date(post_date):
+    """Save the last downloaded post date to the file."""
     with open(LAST_POST_FILE, "w") as file:
-        file.write(post_id)
+        file.write(post_date.strftime('%Y-%m-%d %H:%M:%S'))
 
-def download_new_posts(username, max=0):
+def download_new_posts(username):
     """Download only new Instagram posts."""
     loader = instaloader.Instaloader()
-    global last_fetched_post_id, new_posts_downloaded
-
-    # Login (optional if accessing private profiles)
-    # loader.login("your_username", "your_password")
-
-    # Get the last downloaded post ID
-    last_post_id = get_last_post_id()
-    logging.info(f"Last downloaded post ID: {last_post_id}")
+    global last_post_date
 
     try:
-        # Fetch posts from the user's profile
-        profile = instaloader.Profile.from_username(loader.context, username)
-        for post in profile.get_posts():
-            # Save the ID of the latest post in the current session
-            if last_fetched_post_id is None:
-                last_fetched_post_id = post.shortcode
+        logging.debug(f"Username: {username}")
+        profile = Profile.from_username(loader.context, username)
 
-            # Stop if we reach the last downloaded post
-            if post.shortcode == last_post_id:
-                break
-
-            # Download the post
-            loader.download_post(post, target=profile.username)
-            new_posts_downloaded += 1
-            if max > 0 and new_posts_downloaded >= max:
-                break
-
-        # Update the last downloaded post ID if new posts were found
-        if last_fetched_post_id:
-            save_last_post_id(last_fetched_post_id)
-
-        if new_posts_downloaded > 0:
-            logging.info(f"{new_posts_downloaded} new posts downloaded.")
-            extractfiles('./'+instagram_username)
+        # Set post filter to only download posts after the last downloaded post date
+        if last_post_date:
+            logging.debug(f"Fetch all posts after {last_post_date}")
+            loader.download_profiles({profile}, profile_pic=False, stories=False, post_filter=lambda post: post.date_utc > last_post_date)
         else:
-            logging.warning("No new posts found.")
+            logging.debug("Fetch all posts")
+            loader.download_profiles({profile}, profile_pic=False, stories=False)
 
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
-
-
-# Download Instagram posts
-def download_instagram_posts(username):
-    loader = instaloader.Instaloader()
-    try:
-        # Download posts of the given username
-        print(f"Downloading posts from @{username}...")
-        loader.download_profile(username, profile_pic=False, download_stories=False)
-        print(f"Download complete. Posts saved in the current directory.")
-        return f"./{username}"  # Folder path of the downloaded content
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
+        logging.error(f"An error occurred: {traceback.format_exc()}")
 
 # Parse downloaded Instagram data
-def parse_instagram_data(folder_path):
+def build_post_array(folder_path):
     posts_data = []
     if not os.path.exists(folder_path):
         logging.warning(f"Folder not found: {folder_path}")
@@ -129,92 +94,84 @@ def parse_instagram_data(folder_path):
 
     for root, _, files in os.walk(folder_path):
         for file in files:
-            if file.endswith('.json') and file != "profile.json":
-                with open(os.path.join(root, file), 'r') as json_file:
-                    post_data = json.load(json_file)
-                    image_path = os.path.join(root, file.replace(".json", ".jpg"))
+            if file.endswith('.txt'):
+                with open(os.path.join(root, file), 'r') as txt_file:
+                    post_data = txt_file.readline()
+                    image_path = os.path.join(root, file.replace(".txt", ".jpg"))
                     if os.path.exists(image_path):
                         posts_data.append({
-                            # "caption": post_data.get("edge_media_to_caption", {}).get("edges", [{}])[0].get("node", {}).get("text", ""),
-                            "caption": post_data.get("node", {}).get("caption", ""),
+                            "fname": file,
+                            "caption": post_data,
                             "image_path": image_path,
                         })
     return posts_data
 
 # Upload to WordPress
-def upload_to_wordpress(posts_data, wordpress_url, username, application_password):
-    # Upload media to WordPress
-    credentials = f"{username}:{application_password}"
-    token = base64.b64encode(credentials.encode())
-    authheaders = {"Authorization": f"Basic {token.decode('utf-8')}"}
+def upload_to_wordpress(post, wordpress_url, authheaders):
+    try:
+        with open(post["image_path"], "rb") as image_file:
+            
+            media_response = requests.post(
+                f"{wordpress_url}/wp-json/wp/v2/media",
+                headers=authheaders,
+                files={"file": image_file},
+            )
+            if media_response.status_code == 201:
+                media_id = media_response.json()["id"]
+                # Create WordPress post
+                pattern = r"^#(\w+)\s+(.*)\s+(\d{4}-\d{2}-\d{2})$"
 
-    distilleries = fetch_distilleries(authheaders)
-        
-    for post in posts_data:
-        try:
-            with open(post["image_path"], "rb") as image_file:
-                
-                media_response = requests.post(
-                    f"{wordpress_url}/wp-json/wp/v2/media",
-                    headers=authheaders,
-                    files={"file": image_file},
-                )
-                if media_response.status_code == 201:
-                    media_id = media_response.json()["id"]
-                    # Create WordPress post
-                    pattern = r"^#(\w+)\s+(.*)\s+(\d{4}-\d{2}-\d{2})$"
+                # Match the pattern
+                match = re.match(pattern, post["caption"])
 
-                    # Match the pattern
-                    match = re.match(pattern, post["caption"])
+                if match:
+                    hashtag = match.group(1)
+                    remaining_text = match.group(2)
+                    date = match.group(3)
+                    datet = match.group(3) + f"T{random.randint(0, 23):02d}:{random.randint(0, 59):02d}" + ":00"
 
-                    if match:
-                        hashtag = match.group(1)
-                        remaining_text = match.group(2)
-                        date = match.group(3)
-                        datet = match.group(3) + f"T{random.randint(0, 23):02d}:{random.randint(0, 59):02d}" + ":00"
+                    distillery_name = find_distillery_by_name(distilleries, remaining_text)
 
-                        distillery_name = find_distillery_by_name(distilleries, remaining_text)
+                    post_data = {
+                        "title": remaining_text,
+                        "status": "publish",
+                        "featured_media": media_id,
+                        "date": datet,
+                    }
 
-                        post_data = {
-                            "title": remaining_text,
-                            "status": "publish",
-                            "featured_media": media_id,
-                            "date": datet,
-                        }
+                    if distillery_name:
+                        post_data["distillery"] = distillery_name
 
-                        if distillery_name:
-                            post_data["distillery"] = distillery_name
+                    if hashtag == "in":
+                        post_data["date_bought"] = date
+                    elif hashtag == "open":
+                        post_data["date_opened"] = date
+                    elif hashtag == "out":
+                        post_data["date_finished"] = date
 
-                        if hashtag == "in":
-                            post_data["date_bought"] = date
-                        elif hashtag == "open":
-                            post_data["date_opened"] = date
-                        elif hashtag == "out":
-                            post_data["date_finished"] = date
-
-                        logging.info(f"bits {date}")
-                    else:
-                        post_data = {
-                            "title": post["caption"],
-                            "status": "draft",
-                            "featured_media": media_id,
-                        }
-
-                    post_response = requests.post(
-                        f"{wordpress_url}/wp-json/wp/v2/whisky_bottle",
-                        headers=authheaders,
-                        json=post_data,
-                    )
-                    if post_response.status_code == 201:
-                        logging.info(f"Post uploaded: {post_response.json().get('link')}")
-                    else:
-                        logging.error(f"Failed to create post: {post_response.status_code} - {post_response.text}")
-                        # print(f"Failed to create post: {post_response.status_code}")
+                    logging.info(f"bits {date}")
                 else:
-                    logging.error(f"Failed to upload media: {media_response.status_code} - {media_response.text}")
-                    # print(f"Failed to upload media: {media_response.status_code}")
-        except Exception as e:
-            logging.error(f"An error occurred: {e}")
+                    post_data = {
+                        "title": post["caption"],
+                        "status": "draft",
+                        "featured_media": media_id,
+                    }
+
+                post_response = requests.post(
+                    f"{wordpress_url}/wp-json/wp/v2/whisky_bottle",
+                    headers=authheaders,
+                    json=post_data,
+                )
+                if post_response.status_code == 201:
+                    logging.info(f"Post uploaded: {post_response.json().get('link')}")
+                else:
+                    logging.error(f"Failed to create post: {post_response.status_code} - {post_response.text}")
+                    # print(f"Failed to create post: {post_response.status_code}")
+            else:
+                logging.error(f"Failed to upload media: {media_response.status_code} - {media_response.text}")
+                # print(f"Failed to upload media: {media_response.status_code}")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
 def fetch_distilleries(authheaders):
     """
@@ -250,22 +207,52 @@ def find_distillery_by_name(distilleries, search_string):
 
     return ""
 
+def postDate(e):
+  return e['fname']
+
+def cleanupFiles(path):
+    shutil.rmtree(path, ignore_errors=False, onexc=fileDelHandler)
+
+def fileDelHandler(func, path, exc_info):
+    logging.error(f"Error in cleanup: {exc_info}")
 
 
 # Main script
 if __name__ == "__main__":
-    # Step 1: Download Instagram posts
-    folder = download_instagram_posts(instagram_username)
-    download_new_posts(instagram_username)
-    # new_posts_downloaded is a global
-    # download_new_posts(instagram_username, 5)
-    # new_posts_downloaded = 5
-    if new_posts_downloaded > 0:
-        # Step 2: Parse Instagram data
-        posts = parse_instagram_data('./'+instagram_username)
+    # Get the last downloaded post date
+    last_post_date = get_last_post_date()
+    logging.info(f"Last downloaded post date: {last_post_date} - obj {isinstance(last_post_date, datetime)}")
 
-        # Step 3: Upload to WordPress
-        if posts:
-            upload_to_wordpress(posts, wordpress_url, wp_username, wp_application_password)
-        else:
-            logging.warning("No posts found to upload.")
+    # Step 1: Download Instagram posts
+    download_new_posts(instagram_username)
+
+    # Step 2: Parse Instagram data
+    posts = build_post_array('./'+instagram_username)
+    logging.debug('array built')
+    posts.sort(key=postDate)
+
+    logging.debug('posts sorted')
+    logging.debug(posts)
+
+    # Step 3: Upload to WordPress & cleanup
+    if posts:
+        new_posts_processed = 0
+
+        credentials = f"{wp_username}:{wp_application_password}"
+        token = base64.b64encode(credentials.encode())
+        authheaders = {"Authorization": f"Basic {token.decode('utf-8')}"}
+
+        distilleries = fetch_distilleries(authheaders)
+        for post in posts:
+            upload_to_wordpress(post, wordpress_url, authheaders)
+            last_post_date = datetime.strptime(post['fname'].rsplit('.', 1)[0], "%Y-%m-%d_%H-%M-%S_UTC")
+            new_posts_processed += 1
+
+            if POSTS_PER_INVOKE > 0 and new_posts_processed >= POSTS_PER_INVOKE:
+                break
+        
+        save_last_post_date(last_post_date)
+
+        cleanupFiles('./'+instagram_username)
+    else:
+        logging.warning("No posts found to upload.")
